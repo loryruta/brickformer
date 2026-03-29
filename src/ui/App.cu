14f1b73c67905e6087c04aea8905642da9738bb3
Lorenzo Rutayisire <loryruta23@gmail.com>
2026-03-29 22:47:03 +0100

    app: firebase in .env

--- src/ui/App.cu
+++ src/ui/App.cu
@@ -65,16 +65,18 @@ App::~App()
 
 void App::setup_firebase()
 {
+#ifndef BF_OPENSOURCE
     firebase::AppOptions app_options;
-    app_options.set_api_key("AIzaSyBng97YujpJM-xtkbT4TgU5GHrGKNJpREY");
-    app_options.set_project_id("legobuilder-257af");
-    app_options.set_storage_bucket("legobuilder-257af.firebasestorage.app");
-    app_options.set_messaging_sender_id("930461315909");
-    app_options.set_app_id("1:930461315909:web:972ae535fa5dda50ab042c");
+    app_options.set_api_key(BF_FIREBASE_API_KEY);
+    app_options.set_project_id(BF_FIREBASE_PROJECT_ID);
+    app_options.set_storage_bucket(BF_FIREBASE_STORAGE_BUCKET);
+    app_options.set_messaging_sender_id(BF_FIREBASE_MESSAGING_SENDER_ID);
+    app_options.set_app_id(BF_FIREBASE_APP_ID);
     m_firebase_app = firebase::App::Create(app_options);
     CHECK_STATE(m_firebase_app, "Failed to initialize Firebase app");
     m_firebase_auth = firebase::auth::Auth::GetAuth(m_firebase_app);
     CHECK_STATE(m_firebase_auth, "Failed to initialize Firebase auth");
+#endif
 }
 
 void App::start()
